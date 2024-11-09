c5aee890285a183180d58789c7101001e44504fa
cg <congvc98@gmail.com>
2024-11-09 22:05:33 +0700

    change modal style

--- src/pages/Email.tsx
+++ src/pages/Email.tsx
@@ -64,18 +64,15 @@ const Email = () => {
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <DialogPanel
               transition
-              className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+              className="relative transform overflow-hidden rounded-lg bg-background px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
             >
               <div>
                 <div className="mt-3 text-center sm:mt-5">
-                  <DialogTitle
-                    as="h3"
-                    className="text-base font-semibold text-gray-900"
-                  >
+                  <DialogTitle as="h3" className="t4 font-semibold text-while">
                     {alert?.label || "Label"}
                   </DialogTitle>
                   <div className="mt-2">
-                    <p className="text-sm text-gray-500">
+                    <p className="t4 text-while">
                       {alert?.message || "Message"}
                     </p>
                   </div>
@@ -88,7 +85,7 @@ const Email = () => {
                     $open(false);
                     navigate(internal.home);
                   }}
-                  className="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 t5 font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark"
+                  className="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 t5 font-normal text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark"
                 >
                   Go back to home
                 </button>
