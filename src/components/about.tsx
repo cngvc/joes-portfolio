fd0c7d3b4a4fe27f585afaf52b7b5456678e2ef8
cg <congvc98@gmail.com>
2025-03-21 00:33:17 +0700

    refactor: enhance avatar display with animations and remove tooltips

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,11 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./ui/tooltip";
 import Window from "./window";
 
 const About = () => {
@@ -15,24 +10,28 @@ const About = () => {
     <div className="flex flex-col gap-2 relative bg-background">
       <div className="max-w-[10rem] w-full h-auto absolute right-4 -translate-y-full">
         <div className="w-full flex items-end relative">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <LazyLoadImage
-                  alt="Avatar"
-                  src={
-                    theme === "dark"
-                      ? "/assets/avatar.png"
-                      : "/assets/avatar-light.png"
-                  }
-                  className="object-contain z-50 w-32 md:w-40 h-32 md:h-40"
-                />
-              </TooltipTrigger>
-              <TooltipContent side="left">
-                Hi, Nice to meet you!.
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={theme}
+              initial={{ opacity: 0, y: 120 }}
+              animate={{
+                opacity: [0, 1, 1, 1, 1, 1],
+                y: [120, 80, 80, 0],
+              }}
+              exit={{ opacity: [1, 1, 1, 1, 0], y: [0, 80, 80, 120] }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+            >
+              <LazyLoadImage
+                alt="Avatar"
+                src={
+                  theme === "dark"
+                    ? "/assets/avatar.png"
+                    : "/assets/avatar-light.png"
+                }
+                className="object-contain w-full h-full"
+              />
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
 
